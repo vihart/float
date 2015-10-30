67cd7228e48670afe820982192d95f004ba02758
vihart <vi@vihart.com>
2015-10-30 16:22:36 -0700

    add background landscape and fog

--- js/float.js
+++ js/float.js
@@ -18,18 +18,36 @@ var controls = new THREE.VRControls( camera );
 var effect = new THREE.VREffect( renderer );
 effect.setSize( window.innerWidth, window.innerHeight );
 
+//bg color
+renderer.setClearColor( 0x66aaff );
 
-//skybox
-var skyGeometry = new THREE.IcosahedronGeometry(10,4);
-var skyMaterial = new THREE.MeshBasicMaterial({
-  map: THREE.ImageUtils.loadTexture(
-      "media/officeRender.jpg"),
-  color:0xffffff
-  });
-var skybox = new THREE.Mesh( skyGeometry, skyMaterial );
-skybox.scale.set(100,100,100);
-skybox.material.side = THREE.BackSide;
-scene.add(skybox);
+scene.fog = new THREE.FogExp2( 0xaaddff, .05);
+
+
+// //skybox
+// var skyGeometry = new THREE.IcosahedronGeometry(10,4);
+// var skyMaterial = new THREE.MeshBasicMaterial({
+//   map: THREE.ImageUtils.loadTexture(
+//       "media/officeRender.jpg"),
+//   color:0xffffff
+//   });
+// var skybox = new THREE.Mesh( skyGeometry, skyMaterial );
+// skybox.scale.set(100,100,100);
+// skybox.material.side = THREE.BackSide;
+// scene.add(skybox);
+
+//ground
+var planeGeometry = new THREE.PlaneGeometry( 100, 100, 100, 100 );
+var planeMaterial = new THREE.MeshLambertMaterial( {color: 0x88ff66, side: THREE.DoubleSide, wireframe:false} );
+var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.rotation.x = 1.58;
+plane.position.y = -10;
+plane.scale.set(2,2,2);
+scene.add( plane );
+
+for (var i = 0; i < plane.geometry.vertices.length; i++){
+  plane.geometry.vertices[i].z = 2*Math.sin(plane.geometry.vertices[i].y/3) + 2*Math.sin(plane.geometry.vertices[i].x/4) + 5*Math.cos(plane.geometry.vertices[i].y/7) + 3*Math.cos(plane.geometry.vertices[i].x/5);
+}
 
 //Island 1
 var island = [
@@ -40,6 +58,9 @@ var island = [
 var matArray = [
   new THREE.MeshLambertMaterial({color:0xff8800}), //0: brown ground
   new THREE.MeshLambertMaterial({color:0xaaaaaa}), //1: grey rocks
+  new THREE.MeshLambertMaterial({color:0x33ff00}), //2: green grass
+  new THREE.MeshLambertMaterial({color:0xff8800}), //0: brown ground
+  new THREE.MeshLambertMaterial({color:0xaaaaaa}), //1: grey rocks
   new THREE.MeshLambertMaterial({color:0x33ff00}) //2: green grass
   ];
 
@@ -48,47 +69,133 @@ var fileName = [
   'media/i2ground.obj', 'media/i2rocks.obj', 'media/i2grass.obj'
   ];
 
-  // load the meshes
-for (var j = 0; j < 2; j++){
-  for (var i=0; i<3; i++){
+//apparently loaders work nonlinearly and I really can't loop this or something??
 
-    var manager = new THREE.LoadingManager();
-    var loader = new THREE.OBJLoader(manager);
+//island 0:
+var manager00 = new THREE.LoadingManager();
+var loader00 = new THREE.OBJLoader(manager00);
 
-    loader.load( fileName[i + 3*j], function ( object ) {
-    object.scale.set(1,1,1);
-    object.position.set(0,0,0);    
-    object.traverse(function (child) {
-            if (child instanceof THREE.Mesh) {
-                child.material = matArray[i];
-                child.frustumCulled = false;
-            }
-        });
-    var thing = object;
-    island[1].add(thing);
-
+loader00.load( fileName[0], function ( object ) {
+object.scale.set(1,1,1);
+object.position.set(0,0,0);    
+object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material = matArray[0];
+            child.frustumCulled = false;
+        }
     });
-  }
-}
+var islandBit = object;
+island[0].add(islandBit);
+});
+
+var manager01 = new THREE.LoadingManager();
+var loader01 = new THREE.OBJLoader(manager01);
+
+loader01.load( fileName[1], function ( object ) {
+object.scale.set(1,1,1);
+object.position.set(0,0,0);    
+object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material = matArray[1];
+            child.frustumCulled = false;
+        }
+    });
+var islandBit = object;
+island[0].add(islandBit);
+});
+
+var manager02 = new THREE.LoadingManager();
+var loader02 = new THREE.OBJLoader(manager02);
+
+loader02.load( fileName[2], function ( object ) {
+object.scale.set(1,1,1);
+object.position.set(0,0,0);    
+object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material = matArray[2];
+            child.frustumCulled = false;
+        }
+    });
+var islandBit = object;
+island[0].add(islandBit);
+});
+
 scene.add( island[0] );
+
+//island 1:
+var manager10 = new THREE.LoadingManager();
+var loader10 = new THREE.OBJLoader(manager10);
+
+loader10.load( fileName[3], function ( object ) {
+object.scale.set(1,1,1);
+object.position.set(0,0,0);    
+object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material = matArray[3];
+            child.frustumCulled = false;
+        }
+    });
+var islandBit = object;
+island[1].add(islandBit);
+});
+
+var manager11 = new THREE.LoadingManager();
+var loader11 = new THREE.OBJLoader(manager11);
+
+loader11.load( fileName[4], function ( object ) {
+object.scale.set(1,1,1);
+object.position.set(0,0,0);    
+object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material = matArray[4];
+            child.frustumCulled = false;
+        }
+    });
+var islandBit = object;
+island[1].add(islandBit);
+});
+
+var manager12 = new THREE.LoadingManager();
+var loader12 = new THREE.OBJLoader(manager12);
+
+loader12.load( fileName[5], function ( object ) {
+object.scale.set(1,1,1);
+object.position.set(0,0,0);    
+object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material = matArray[5];
+            child.frustumCulled = false;
+        }
+    });
+var islandBit = object;
+island[1].add(islandBit);
+});
+
 scene.add( island[1] );
-island[1].position.y = -3
 
 //lights    
-var light = new THREE.PointLight( 0xffffff, 1, 20);
-light.position.set( 0,5,0);
+var light = new THREE.PointLight( 0xffffff, 1, 40);
+light.position.set( 0,4,0);
 light.castShadow = true;
 scene.add( light );
 
+var light2 = new THREE.PointLight( 0xffffff, 1, 100);
+light2.position.set( 30,10,20);
+light2.castShadow = true;
+scene.add( light2 );
+
 var t = 0;
+
+// island[0].position.y = -10;
+island[1].position.z = -1;
 /*
 Request animation frame loop function
 */
 function animate() {
 
-  t += .001
+  t += .005
 
-  island[1].position.z = Math.sin(t);
+  island[1].position.x = 10 + (10 * Math.sin(t));
 
 
   //Update VR headset position and apply to camera.
--- js/vr/VRControls.js
+++ js/vr/VRControls.js
@@ -136,7 +136,7 @@ THREE.VRControls = function ( camera, speed, done ) {
 	// the Rift SDK returns the position in meters
 	// this scale factor allows the user to define how meters
 	// are converted to scene units.
-	this.scale = 10;
+	this.scale = 12;
 
 	this.enableGamepad = function(isGamepad) {
 		this.isGamepad = isGamepad;
