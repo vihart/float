d708f3a1de0f5bb38241118ffd0476dbc41e7beb
Vi Hart <vi@vihart.com>
2016-06-02 15:19:37 -0700

    arrow keys and i/k to position everything

--- js/float.js
+++ js/float.js
@@ -1615,6 +1615,18 @@ function onkey(event) {
     controls.resetSensor(); //zero rotation
   } else if (event.keyCode == 70 || event.keyCode == 13) { //f or enter
     effect.setFullScreen(true) //fullscreen
+  } else if (event.keyCode == 38) { //up arrow
+    everything.position.x += 1; //
+  } else if (event.keyCode == 40) { //down arrow
+    everything.position.x -= 1; //
+  } else if (event.keyCode == 37) { //left arrow
+    everything.position.z += 1; //
+  } else if (event.keyCode == 39) { //right arrow
+    everything.position.z -= 1; //
+  } else if (event.keyCode == 73) { //i
+    everything.position.y += 1; //
+  } else if (event.keyCode == 75) { //k
+    everything.position.y -= 1; //
   } else if (event.keyCode == 32){ //space
     intro.play();
   } else if (event.keyCode == 80){ //p
